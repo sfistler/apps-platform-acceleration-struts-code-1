31e9a7de1d3226b960780b4cd01c155ea6145eff
sfistler <s.fistler@gmail.com>
2018-04-19 11:53:49 -0600

    struts

---
+++ src/main/java/org/superbiz/struts/Index.java
@@ -0,0 +1,10 @@
+package org.superbiz.struts;
+
+import com.opensymphony.xwork2.ActionSupport;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Index extends ActionSupport {
+
+
+}
